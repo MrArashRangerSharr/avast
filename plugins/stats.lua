5668902ce4c32a4e882b829fdd59b5b2092a3ac6
Mr Client <arashnomiri@gmail.com>
2016-05-27 09:45:21 +0000

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'avast' then -- Put everything you like :)
+  if matches[1]:lower() == 'asimo' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /avast ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /asimo ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -120,7 +120,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "avast" then -- Put everything you like :)
+    if matches[2] == "asimo" then -- Put everything you like :)
       if not is_admin(msg) then
         return "فقط مدیران !"
       else
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (avast)",-- Put everything you like :)
-		"^[!/]([Aa]vast)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (asimo)",-- Put everything you like :)
+		"^[!/]([Aa]simo)"-- Put everything you like :)
     }, 
   run = run
 }
