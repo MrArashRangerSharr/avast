a416202e92a0e803b79f6293a5e738a7346003ce
Mr Client <arashnomiri@gmail.com>
2016-05-30 18:35:10 +0000

    Create inamir.lua

---
+++ plugins/inamir.lua
@@ -0,0 +1,16 @@
+do
+ function run(msg, matches)
+return [[ 
+نام: امیر
+مقام: مدير ربات و بخش پوشش مالی
+شماره تلفن: +989114661736
+Telegram.me/Sudo_Vira_DrCyber
+]]
+end
+return {
+patterns = {
+"^card @sudo_vira_drcyber$",
+},
+run = run
+}
+end
